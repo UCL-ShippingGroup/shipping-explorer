850c604f4bbfd0260bcef613a29b38c43b222d21
james <61799526+james-stewart-808@users.noreply.github.com>
2026-08-08 17:34:02 +0100

    Remove trade page from Streamlit app navigation
    Removed trade page from navigation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,13 @@ st.markdown(
 # Define the pages
 main = st.Page("main.py", title="Overview", icon="🏡")
 inventories = st.Page("inventories.py", title="Voyage Inventories", icon="🚢")
-trade = st.Page("trade.py", title="Seaborne Trade Portfolios", icon="📦")
+#trade = st.Page("trade.py", title="Seaborne Trade Portfolios", icon="📦")
 #impact_tracking = st.Page("impact_tracking.py", title="NZF Economic Impacts", icon="💵")
 #costs_of_climate_change = st.Page("costs_of_climate_change.py", title="Costs of Climate Change", icon="💰")
 contact = st.Page("contact.py", title="Contact", icon="📒")
 
 # Set up navigation
-pg = st.navigation([main, inventories, trade, contact])
+pg = st.navigation([main, inventories, contact])
 
 # Run the selected page
 pg.run()
