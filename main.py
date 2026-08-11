a901dcbd05232dd8fccd3e26fbeb8b7919ba1c0f
yosephis <144414930+yosephis@users.noreply.github.com>
2026-08-11 11:19:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ st.session_state.iso_code = country_iso_codes[(country_iso_codes.iso_country == 
 
 st.write("""Use the sidebar to explore the different components of the dashboard...""")
 st.page_link("inventories.py", label="**Voyage-based Inventories**", icon="🚢")
-st.page_link("trade.py", label="**Merchandise Trade Portfolios**", icon="📦")
+#st.page_link("trade.py", label="**Merchandise Trade Portfolios**", icon="📦")
 
 st.divider()
 
