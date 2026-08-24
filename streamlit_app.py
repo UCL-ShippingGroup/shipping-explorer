c8b258a4aa5cf7887ea4b0b48673593aefccc0d0
yosephis <144414930+yosephis@users.noreply.github.com>
2026-08-24 02:22:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.markdown(
 
 # Define the pages
 main = st.Page("main.py", title="Overview", icon="🏡")
-global = st.Page("global.py", title="Global Outlook", icon="🚢")
+globa = st.Page("global.py", title="Global Outlook", icon="🚢")
 inventories = st.Page("inventories.py", title="Voyage Inventories", icon="🚢")
 #trade = st.Page("trade.py", title="Seaborne Trade Portfolios", icon="📦")
 #impact_tracking = st.Page("impact_tracking.py", title="NZF Economic Impacts", icon="💵")
@@ -31,7 +31,7 @@ inventories = st.Page("inventories.py", title="Voyage Inventories", icon="🚢")
 contact = st.Page("contact.py", title="Contact", icon="📒")
 
 # Set up navigation
-pg = st.navigation([main, global, inventories, contact])
+pg = st.navigation([main, globa, inventories, contact])
 
 # Run the selected page
 pg.run()
