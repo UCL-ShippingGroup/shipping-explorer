e3a0f0c1ea7c2df2f44d70d92846f01b9d7402f8
yosephis <144414930+yosephis@users.noreply.github.com>
2026-08-26 07:47:33 +0100

    Update regional.py

--- regional.py
+++ regional.py
@@ -11,14 +11,14 @@ df_in = pd.read_csv("https://raw.githubusercontent.com/UCL-ShippingGroup/shippin
 df_reg = pd.read_csv("https://raw.githubusercontent.com/UCL-ShippingGroup/shipping-explorer/main/datasets/country_status_labels_v3.csv")
 
 df = df_in.merge(
-    df_reg[["iso-3", "region_wb", "status"]],
+    df_reg[["iso_3", "region_wb", "status"]],
     left_on="alpha-3",
-    right_on="iso-3",
+    right_on="iso_3",
     how="left"
 )
 
 # Remove the duplicate ISO-3 column from df_reg
-df = df.drop(columns="iso-3")
+df = df.drop(columns="iso_3")
 
 # ---------------------------------------------------------
 # Indicator control
